20c9e193fe7adf4b8f56fa13703e6bfe9c655d6f
SoleeL <carlosnr3005@gmail.com>
2020-03-16 23:56:20 -0300

    2.- Eliminacion de archivos

--- app/categorias/routes.py
+++
@@ -1,9 +0,0 @@
-from . import categorias_bp
-from .models import Categoria
-
-
-@categorias_bp.route("/crearCategoria/<string:nombre>")
-def index(nombre):
-    c = Categoria(nombre=nombre)
-    c.save()
-    return "Categoria insertada correctamente:<br>"+str(repr(c))
--- config.py
+++
@@ -1,6 +0,0 @@
-DEBUG = True
-TESTING = True
-SQLALCHEMY_DATABASE_URI = 'postgresql://moran:moran@192.168.0.250/software' 
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SECRET_KEY = "\xcd\xa73\x05\x07\xa9\x83+\xf7\xa0\xc9g\xb5\x8bY@"
-USE_SESSION_FOR_NEXT = True
